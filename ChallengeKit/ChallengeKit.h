fc9b85e1def465ecbb3d1bd088c6ed93e1c7df39
Diana Gren <dianagren@gmail.com>
2015-04-08 20:53:30 -0700

    Add ChallengeKit.

---
+++ ChallengeKit/ChallengeKit.h
@@ -0,0 +1,19 @@
+//
+//  ChallengeKit.h
+//  ChallengeKit
+//
+//  Created by Diana Gren on 4/8/15.
+//  Copyright (c) 2015 Diana Gren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChallengeKit.
+FOUNDATION_EXPORT double ChallengeKitVersionNumber;
+
+//! Project version string for ChallengeKit.
+FOUNDATION_EXPORT const unsigned char ChallengeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChallengeKit/PublicHeader.h>
+
+
