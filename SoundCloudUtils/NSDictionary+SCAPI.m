8f1cee51f491d1ae2efad53fa31d0f2c25e2592c
Diana Gren <dianagren@gmail.com>
2015-04-25 16:57:00 -0700

    Change author name.

--- SoundCloudUtils/NSDictionary+SCAPI.m
+++ SoundCloudUtils/NSDictionary+SCAPI.m
@@ -2,8 +2,8 @@
 //  NSDictionary+SCAPI.m
 //  Challenge
 //
-//  Created by Diana Gren on 4/25/15.
-//  Copyright (c) 2015 Diana Gren. All rights reserved.
+//  Created by D Gren on 4/25/15.
+//  Copyright (c) 2015 D Gren. All rights reserved.
 //
 
 #import "NSDictionary+SCAPI.h"
--- SoundCloudUtilsTests/SCRequestTest.m
+++ SoundCloudUtilsTests/SCRequestTest.m
@@ -2,8 +2,8 @@
 //  SCRequestTest.m
 //  Challenge
 //
-//  Created by Diana Gren on 4/25/15.
-//  Copyright (c) 2015 Diana Gren. All rights reserved.
+//  Created by D Gren on 4/25/15.
+//  Copyright (c) 2015 D Gren. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
@@ -13,7 +13,6 @@
 #import "SCRequest.h"
 
 @interface SCRequestTest : XCTestCase
-
 @end
 
 @implementation SCRequestTest
